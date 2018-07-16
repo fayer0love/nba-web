7152ee7b76bb3945683b6f51be1b2b3924b20eae
Shiyang Li <sli99@hawk.iit.edu>
2018-07-15 19:24:00 -0500

    search bar

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,7 @@
 export const PROFILE_PIC_URL_PREFIX = 'http://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190';
 export const TEAM_LOGO_URL_PREFIX = 'http://stats.nba.com/media/img/teams/logos';
+export const DEFAULT_PLAYER_INFO = {
+  playerId: 201939,
+  fullName: 'Stephen Curry',
+  teamAbbreviation: 'GSW',
+};
